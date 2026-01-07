9f2fa61be610bc9f43c2da13d67595a3cbf25367
abinashrout001 <abinashrout.mail@gmail.com>
2026-01-07 11:35:12 +0530

    footer

--- src/pages/publicsite/Publicsite.jsx
+++ src/pages/publicsite/Publicsite.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PublicHeader from "../../components/publiclayout/PublicHeader";
 
 import { Outlet } from "react-router-dom";
-import Footer from "../../components/Footer/footer";
+import Footer from "../../components/Footer/Footer";
 const Publicsite = () => {
   return (
     <div>
