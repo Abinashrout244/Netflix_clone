baa11aca265ae742c13d0becbdeb1be744f5fadb
abinashrout001 <abinashrout.mail@gmail.com>
2026-01-07 11:31:58 +0530

    apply the chnage

--- src/pages/publicsite/Publicsite.jsx
+++ src/pages/publicsite/Publicsite.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PublicHeader from "../../components/publiclayout/PublicHeader";
-import PublicBody from "../../components/publiclayout/publicBody";
+
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/Footer/footer";
 const Publicsite = () => {
